115a2c9516d07a1271c36e14c08a5b0114cf1ee8
Hassnain Ali <hassnainali610@gmail.com>
2021-06-23 16:42:01 +0500

    Update User.sql

--- OnlineStore/OnlineStore.Database/dbo/Tables/User.sql
+++ OnlineStore/OnlineStore.Database/dbo/Tables/User.sql
@@ -1,13 +1,18 @@
 ﻿CREATE TABLE [dbo].[User] (
-    [Id]          INT           IDENTITY (1, 1) NOT NULL,
-    [FirstName]   NVARCHAR (50) NOT NULL,
-    [LastName]    NVARCHAR (50) NOT NULL,
-    [Email]       NVARCHAR (50) NOT NULL,
-    [Country]     NVARCHAR (50) NOT NULL,
-    [PhoneNumber] NVARCHAR (50) NOT NULL,
-    [Password]    NVARCHAR (50) NOT NULL,
+    [Id]             INT           IDENTITY (1, 1) NOT NULL,
+    [FirstName]      NVARCHAR (50) NOT NULL,
+    [LastName]       NVARCHAR (50) NOT NULL,
+    [Email]          NVARCHAR (50) NOT NULL,
+    [Country]        NVARCHAR (50) NOT NULL,
+    [PhoneNumber]    NVARCHAR (50) NOT NULL,
+    [Password]       NVARCHAR (50) NOT NULL,
+    [Role]           NVARCHAR (50) CONSTRAINT [DF_User_Role] DEFAULT (user_name()) NOT NULL,
+    [ProfilePicture] NVARCHAR (50) NOT NULL,
+    [CreatedDate]    DATETIME      NOT NULL,
     CONSTRAINT [PK__User__3214EC0761E3D1E6] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
 
 
